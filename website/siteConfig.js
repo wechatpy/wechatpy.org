c058d6cec3dd1ec6e2e2f47648d8bebdae76f906
messense <messense@icloud.com>
2020-03-22 15:15:48 +0800

    Add cname

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -110,6 +110,7 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   repoUrl: 'https://github.com/wechatpy/wechatpy',
+  cname: 'www.wechatpy.org',
 };
 
 module.exports = siteConfig;
